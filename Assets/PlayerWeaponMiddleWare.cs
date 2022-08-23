f1255b2a55e738c61a19295574ae8e5e0974341f
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-08-23 10:55:34 -0700

    second

--- Assets/PlayerWeaponMiddleWare.cs
+++ Assets/PlayerWeaponMiddleWare.cs
@@ -6,11 +6,13 @@ public class PlayerWeaponMiddleWare : MonoBehaviour
 {
     public PlayerBehaviour pb;
     public PlayerWeaponSettings playerWeaponSettings;
+    public GameObject enemy;
     public int currentWeaponIndex;
     void Start()
     {
         pb = this.GetComponent<PlayerBehaviour>();
         playerWeaponSettings = pb.weaponSettings;
+        Instantiate(enemy);
     }
 
     // Update is called once per frame
